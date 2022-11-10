3d3d7cf7f51423f2ac2425f292ac47cd88d3226c
Niklas Sheth <niklassheth@gmail.com>
2022-11-10 18:28:08 -0500

    Use ResponseBuilder to generate response

--- src/main/java/org/hypergolic/PlayerResource.java
+++ src/main/java/org/hypergolic/PlayerResource.java
@@ -2,14 +2,16 @@ package org.hypergolic;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.net.URI;
 
 @Path("/player")
 public class PlayerResource {
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     @Path("{id: \\d+}")
     public Uni<Player> getPlayerByID(@PathParam("id") long id) {
         return Player.findById(id);
@@ -17,8 +19,8 @@ public class PlayerResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Uni<String> addPlayer(Player p) {
-        return Panache.<Player>withTransaction(p::persistAndFlush)
-                .map(inserted -> inserted.id + "");
+    public Uni<RestResponse<Player>> addPlayer(Player p) {
+        return Panache.<Player>withTransaction(p::persist)
+                .map(player -> (ResponseBuilder.<Player>created(URI.create("/player/"+player.id)).entity(p).build()));
     }
 }
