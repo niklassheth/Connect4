b9ff92d65bea3b371ccfa404c1c5cd13d454a21c
Niklas Sheth <niklassheth@gmail.com>
2022-11-24 23:02:56 -0500

    work in progress

--- src/main/frontend/src/components/Connect4/Connect4.tsx
+++ src/main/frontend/src/components/Connect4/Connect4.tsx
@@ -1,13 +1,21 @@
 import './Connect4.css';
 import Board from '../Board/Board';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import type { Move } from '../Board/Board'
+import type WebSocketEventMap from 'react-use-websocket';
 
 function Connect4() {
 
+  const { sendMessage } = useWebSocket("ws://localhost:8080/connection/1", {
+    onMessage: (event) => {
+      let moves = JSON.parse(event.data);
+      console.log(moves);
+      setMoves(moves);
+    }
+  });
   const [moves, setMoves] = useState<Move[]>([]);
-
-  const handleMove = (move: Move): void => setMoves([...moves, move]);
+  const handleMove = (move: Move): void => sendMessage(JSON.stringify(move));
 
   return <Board cols={6} rows={7} moves={moves} initialColor={"red"} clickHandler={handleMove}></Board>
 }
--- src/main/java/org/hypergolic/resource/ConnectionResource.java
+++ src/main/java/org/hypergolic/resource/ConnectionResource.java
@@ -1,9 +1,15 @@
 package org.hypergolic.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.panache.common.Sort;
+import org.hypergolic.model.Game;
 import org.hypergolic.model.Move;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.OrderBy;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import javax.ws.rs.Path;
@@ -17,11 +23,17 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionResource {
 
     ConcurrentHashMap<Integer, Session> sessions = new ConcurrentHashMap<>();
+    Game game;
 
     @OnOpen
     public void onOpen(Session session, @PathParam("id") int id) {
         System.out.println(id + " Connected");
+        System.out.println(session.getId());
         sessions.put(id, session);
+        if (game == null) {
+            game = new Game();
+            Panache.withTransaction(game::persist).await().indefinitely();
+        }
     }
 
 
@@ -40,21 +52,24 @@ public class ConnectionResource {
     @OnMessage
     public void onMessage(String message, @PathParam("id") int id) {
         System.out.println("MESSAGE RECEIVED: " + message);
-        if (message.equals("HI"))
-            return;
+        JsonMapper mapper = new JsonMapper();
         try {
-            Move move = new ObjectMapper().readValue(message, Move.class);
-            sessions.forEach((k, v) -> {
-                System.out.println(k);
-                if (k == id) {
-                    return;
-                }
-                System.out.println("sending...");
-                v.getAsyncRemote().sendText(message);
-            });
-        } catch(Exception e) {
-            System.out.println("uh oh: " + message);
+            Move move = mapper.readValue(message, Move.class);
+            move.game = game;
+            Panache.withTransaction(move::persist).await().indefinitely();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
+        sessions.forEach((k, v) -> {
+            try {
+                //Game g = Game.<Game>findById(game.id).await().indefinitely();
+                var moves = Move.<Move>find("game", Sort.ascending("moveNumber"), game).list().await().indefinitely();
+                v.getAsyncRemote().sendText(mapper.writeValueAsString(moves));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
     }
 
 
