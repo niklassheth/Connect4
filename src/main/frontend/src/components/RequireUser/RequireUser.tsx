9ef9266a96f9e202f724d52d51a5eb4d6e73d856
AnonDrew <segdrew328@gmail.com>
2022-12-11 22:32:23 -0500

    fixed page refresh redirect to signup.

--- src/main/frontend/src/components/RequireUser/RequireUser.tsx
+++ src/main/frontend/src/components/RequireUser/RequireUser.tsx
@@ -1,9 +1,14 @@
 import {useUser} from "../UserProvider/UserProvider";
 import {Navigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export default function RequireUser({ children }) {
     const { user } = useUser();
+
     if (!user) {
+        if (localStorage.getItem('user')) {
+            return <Navigate to="/" replace />;
+        }
         return <Navigate to="/signup" replace />;
     }
 
